fec6118733a2bbd5e62719bdfcc7e6f694ae8771
Sally Steuterman <sally.steuterman@gmail.com>
2018-10-10 12:49:56 -0500

    Fixed rocket fill

--- main.py
+++ main.py
@@ -58,13 +58,15 @@ class Rocket:
         self.turtle.penup()
         self.turtle.setposition(self.CIRCLE_CORDS)
         self.turtle.pendown()
+        self.turtle.fillcolor(53, 74, 95)
+        self.turtle.begin_fill()
 
         #draw the circle
         self.turtle.circle(self.RADIUS)
         self.turtle.end_fill()
 
     def draw_curve(self, turtle, angle, dist):
-        for i in range(62):
+        for i in range(63):
             turtle.forward(dist)
             turtle.right(angle)
 
@@ -77,12 +79,14 @@ class Rocket:
         self.turtle.setposition(0, -225)
         self.turtle.setheading(90)
         self.turtle.pendown()
+        self.turtle.fillcolor(244, 119, 83)
+        self.turtle.begin_fill()
 
         # begin drawing
         self.turtle.right(self.PANGLE)
         self.turtle.forward(self.INIT_LENGTH)
 
-        #draw the right fin
+        # draw the right fin
         self.turtle.setheading(0)
         self.turtle.right(55)
         self.turtle.forward(45)
@@ -92,16 +96,22 @@ class Rocket:
         self.turtle.forward(40)
 
         self.turtle.penup()
+        # self.turtle.end_fill()
         self.turtle.setposition(self.PXCORD, self.YCORD)
         self.turtle.setheading(90)
         self.turtle.pendown()
+        # self.turtle.fillcolor("orange")
+        # self.turtle.begin_fill()
         self.draw_curve(self.turtle, self.NCURVE, self.DIST)
+        # self.turtle.end_fill()
 
         # draw the other half of the flame
         self.turtle.penup()
         self.turtle.setposition(0, -225)
         self.turtle.setheading(90)
         self.turtle.pendown()
+        # self.turtle.fillcolor(92,148,206)
+        # self.turtle.begin_fill()
 
         # begin drawing
         self.turtle.right(self.NANGLE)
@@ -118,13 +128,23 @@ class Rocket:
 
         # draw the left curve
         self.turtle.penup()
+        # self.turtle.end_fill()
+        # self.turtle.fillcolor("orange")
+        # self.turtle.begin_fill()
         self.turtle.setposition(self.NXCORD, self.YCORD)
         self.turtle.setheading(90)
         self.turtle.pendown()
 
         self.draw_curve(self.turtle, self.PCURVE, self.DIST)
+        self.turtle.end_fill()
         self.draw_porthole()
+
+        # self.turtle.fillcolor("orange")
+        # self.turtle.begin_fill()
+
         self.draw_flame()
+
+        # self.turtle.end_fill()
 
     # draw_flame
     def draw_flame(self):
@@ -147,7 +167,7 @@ class Rocket:
 
         self.turtle.forward(65)
 
-        self.turtle.width(1)
+        self.turtle.width(12)
         self.turtle.setheading(270)
         self.turtle.penup()
         self.turtle.setposition(15, -115)
@@ -199,7 +219,7 @@ class Planet:
 
 def main():
     #TODO: let user pick bg color
-    BGCOLOR = (53, 74, 95)
+    BGCOLOR = ("white")
 
     wn = turtle.Screen()
     wn.setup(575, 800)
@@ -210,40 +230,40 @@ def main():
     artist.speed(10)
 
     #TODO: let user decide where rocket goes, left, right middle, high, low
-    rocket = Rocket(artist, "white")
+    rocket = Rocket(artist, "black")
     rocket.draw()
 
     side_of_screen = "left" #input("What direction should the star go?")
     #TODO: simply star draw function
-    star = Star(artist, random.randrange(8, 20), (random.randrange(256), random.randrange(256), random.randrange(256)), 200, side_of_screen)
-    star.draw()
-
-    mercury = Planet(artist, "light gray", 5, (150, 250))
-    mercury.draw_outline()
-
-    venus = Planet(artist, "tan", 7, (150, 200))
-    venus.draw_outline()
-
-    earth = Planet(artist, "green", 15, (150,125))
-    earth.draw_outline()
-
-    mars = Planet(artist, "red", 10, (150, 55))
-    mars.draw_outline()
-
-    jupiter = Planet(artist, "dark orange", 30, (150, -30))
-    jupiter.draw_outline()
-
-    saturn = Planet(artist, "lemon chiffon", 25, (150, -100))
-    saturn.draw_outline()
-
-    uranus = Planet(artist, "light blue", 10, (150, -175))
-    uranus.draw_outline()
-
-    neptune = Planet(artist, "blue", 7, (150, -250))
-    neptune.draw_outline()
-
-    pluto = Planet(artist, "rosy brown", 3, (150, -300))
-    pluto.draw_outline()
+    # star = Star(artist, random.randrange(8, 20), (random.randrange(256), random.randrange(256), random.randrange(256)), 200, side_of_screen)
+    # star.draw()
+    #
+    # mercury = Planet(artist, "light gray", 5, (150, 250))
+    # mercury.draw_outline()
+    #
+    # venus = Planet(artist, "tan", 7, (150, 200))
+    # venus.draw_outline()
+    #
+    # earth = Planet(artist, "green", 15, (150,125))
+    # earth.draw_outline()
+    #
+    # mars = Planet(artist, "red", 10, (150, 55))
+    # mars.draw_outline()
+    #
+    # jupiter = Planet(artist, "dark orange", 30, (150, -30))
+    # jupiter.draw_outline()
+    #
+    # saturn = Planet(artist, "lemon chiffon", 25, (150, -100))
+    # saturn.draw_outline()
+    #
+    # uranus = Planet(artist, "light blue", 10, (150, -175))
+    # uranus.draw_outline()
+    #
+    # neptune = Planet(artist, "blue", 7, (150, -250))
+    # neptune.draw_outline()
+    #
+    # pluto = Planet(artist, "rosy brown", 3, (150, -300))
+    # pluto.draw_outline()
 
     input()
 

d55518a1197489c39a71c3892e725816a93d868c
Blake Mills <welzie@gmail.com>
2018-10-10 09:05:17 -0500

    trying to give rocket size and location options

--- main.py
+++ main.py
@@ -86,6 +86,41 @@ class Star:
 
         self.turtle.end_fill()
 
+class RelativeRocket:
+    def __init__(self, turtle, color, size, xcoord, ycoord):
+        self.turtle = turtle
+        self.color = color
+        self.xcoord = xcoord
+        self.ycoord = ycoord
+        if size.lower() == "large":
+            self.modifier = 1
+        else:
+            self.modifier = .5
+        print("drawing rocket size", size, "modifier", self.modifier)
+
+    # draw_init draw the initial outline
+    def draw(self):
+        self.draw_side(self.turtle, "left")
+        self.draw_side(self.turtle, "right")
+
+    def draw_side(self, turtle, side):
+        curve_angle = -0.8 if side == "left" else 0.8
+        turn_angle = -15 if side == "left" else 15
+        heading = 225 if side == "left" else 322
+        self.turtle.penup()
+        self.turtle.setposition(self.xcoord, self.ycoord)
+        self.turtle.setheading(heading)
+        self.turtle.pencolor(self.color)
+        self.turtle.pendown()
+        self.draw_curve(self.turtle, angle=curve_angle, dist=2.3)
+        self.turtle.right(turn_angle)
+        self.turtle.forward(200)
+
+    def draw_curve(self, turtle, angle, dist):
+        for i in range(62):
+            turtle.forward(dist)
+            turtle.right(angle)    
+
 class Rocket:
     PXCORD = 58.4743409445
     NXCORD = -58.4743409445
@@ -99,13 +134,77 @@ class Rocket:
     CIRCLE_CORDS = (10.66, -30)
     RADIUS = 19
     
-    def __init__(self, turtle, color):
+    def __init__(self, turtle, color, size, xcoord, ycoord):
         self.turtle = turtle
         self.color = color
-    
-    #draws the porthole window
-    def draw_porthole(self):
+        self.xcoord = xcoord
+        self.ycoord = ycoord
+        if size.lower() == "large":
+            self.modifier = 1
+        else:
+            self.modifier = .5
+        print("drawing rocket size", size, "modifier", self.modifier)
 
+    # draw_init draw the initial outline
+    def draw(self):
+        self.turtle.pencolor(self.color)
+        self.turtle.width(12 * self.modifier)
+        self.turtle.penup()
+        self.turtle.setposition(self.xcoord, self.ycoord)
+        self.turtle.setheading(90)
+        self.turtle.pendown()
+        self.turtle.begin_fill()
+
+        # begin drawing
+        self.turtle.right(self.PANGLE)
+        self.turtle.forward(self.INIT_LENGTH * self.modifier)
+
+        #draw the right fin
+        self.turtle.setheading(0)
+        self.turtle.right(55)
+        self.turtle.forward(45 * self.modifier)
+        self.turtle.right(55)
+        self.turtle.forward(80 * self.modifier)
+        self.turtle.right(130)
+        self.turtle.forward(40 * self.modifier)
+
+        self.turtle.penup()
+        self.turtle.setposition(self.PXCORD, self.YCORD)
+        self.turtle.setheading(90)
+        self.turtle.pendown()
+        self.draw_curve(self.turtle, self.NCURVE, self.DIST * self.modifier)
+
+        # draw the other half of the flame
+        self.turtle.penup()
+        self.turtle.setposition(0, -225)
+        self.turtle.setheading(90)
+        self.turtle.pendown()
+
+        # begin drawing
+        self.turtle.right(self.NANGLE)
+        self.turtle.forward(self.INIT_LENGTH * self.modifier)
+
+        #draw the left fin
+        self.turtle.setheading(180)
+        self.turtle.left(55)
+        self.turtle.forward(45 * self.modifier)
+        self.turtle.left(55)
+        self.turtle.forward(80 * self.modifier)
+        self.turtle.left(130)
+        self.turtle.forward(40 * self.modifier)
+
+        # draw the left curve
+        self.turtle.penup()
+        self.turtle.setposition(self.NXCORD, self.YCORD)
+        self.turtle.setheading(90)
+        self.turtle.pendown()
+
+        self.draw_curve(self.turtle, self.PCURVE, self.DIST * self.modifier)
+        self.turtle.end_fill()
+        self.draw_porthole()
+        self.draw_flame()
+
+    def draw_porthole(self):
         #make the fill and change the starting position of the turtle
         self.turtle.penup()
         self.turtle.setposition(self.CIRCLE_CORDS)
@@ -120,65 +219,6 @@ class Rocket:
             turtle.forward(dist)
             turtle.right(angle)
 
-    # draw_init draw the initial outline
-    def draw(self):
-
-        self.turtle.pencolor(self.color)
-        self.turtle.width(12)
-        self.turtle.penup()
-        self.turtle.setposition(0, -225)
-        self.turtle.setheading(90)
-        self.turtle.pendown()
-
-        # begin drawing
-        self.turtle.right(self.PANGLE)
-        self.turtle.forward(self.INIT_LENGTH)
-
-        #draw the right fin
-        self.turtle.setheading(0)
-        self.turtle.right(55)
-        self.turtle.forward(45)
-        self.turtle.right(55)
-        self.turtle.forward(80)
-        self.turtle.right(130)
-        self.turtle.forward(40)
-
-        self.turtle.penup()
-        self.turtle.setposition(self.PXCORD, self.YCORD)
-        self.turtle.setheading(90)
-        self.turtle.pendown()
-        self.draw_curve(self.turtle, self.NCURVE, self.DIST)
-
-        # draw the other half of the flame
-        self.turtle.penup()
-        self.turtle.setposition(0, -225)
-        self.turtle.setheading(90)
-        self.turtle.pendown()
-
-        # begin drawing
-        self.turtle.right(self.NANGLE)
-        self.turtle.forward(self.INIT_LENGTH)
-
-        #draw the left fin
-        self.turtle.setheading(180)
-        self.turtle.left(55)
-        self.turtle.forward(45)
-        self.turtle.left(55)
-        self.turtle.forward(80)
-        self.turtle.left(130)
-        self.turtle.forward(40)
-
-        # draw the left curve
-        self.turtle.penup()
-        self.turtle.setposition(self.NXCORD, self.YCORD)
-        self.turtle.setheading(90)
-        self.turtle.pendown()
-
-        self.draw_curve(self.turtle, self.PCURVE, self.DIST)
-        self.draw_porthole()
-        self.draw_flame()
-
-    # draw_flame
     def draw_flame(self):
 
         # specify color and width
@@ -221,8 +261,6 @@ class Rocket:
         self.turtle.penup()
 
         self.turtle.hideturtle()
-
-
 #endregion
 
 #region functions
@@ -242,9 +280,14 @@ def main():
     ground = Ground(turtle=artist, color=DARK_GREEN, screen_width=1000, screen_height=700, height=225)
     ground.draw()
 
+    rocket2 = RelativeRocket(artist, "white", size="large", xcoord=200, ycoord=200)
+    rocket2.draw()
+
     #TODO: let user decide: left, right middle, high, low, big, small, going up, going down
-    rocket = Rocket(artist, "white")
+    rocket = Rocket(artist, "white", size="large", xcoord=0, ycoord=-225)
     rocket.draw()
+
+
 
     star = Star(turtle=artist, size="large", color="white", height="low", side="left")
     star.draw()
@@ -254,6 +297,8 @@ def main():
     star3.draw()
     star4 = Star(turtle=artist, size="random", color="random", height="medium", side="middle")
     star4.draw()
+
+    #TODO: draw random number of stars in random places and colors, use a loop
 
     #screen.exitonclick()
     input()
